2336cae9b4a8ffa3f9ce6ebf3f2b8fadca7b8536
Roberto <mrrobertrausch@gmail.com>
2022-10-28 04:44:01 +0100

    define and declare functions

---
+++ functions.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// definition
+
+void print_string(string);
+void print_string_pyramid(string);
+
+// main
+
+int main(){
+
+	string user_input;
+	cout << "Value: ";
+	getline(cin, user_input);
+	print_string(user_input);
+	print_string_pyramid(user_input);
+}
+
+// declaration
+
+void print_string(string string_input){
+	cout << "\n" << string_input <<"\n";
+}
+
+void print_string_pyramid(string string_pyramid){
+	int counter;
+	for (counter; counter <= string_pyramid.size(); ++counter){
+		cout << string_pyramid.substr(0,counter)<<endl;
+	}
+}
+	
+
+
+
+
